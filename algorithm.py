c7365a63592fbb478c466094217b0ecca6165044
adose <ado.semo@gmail.com>
2021-10-09 06:47:19 +0200

    force calculator

--- algorithm.py
+++ algorithm.py
@@ -11,7 +11,8 @@ class Body:
         self.y = y
         self.vx = vx
         self.vy = vy
-        self.node_chain = []
+        self.Fx = 0
+        self.Fy = 0
 
 
 class Tree:
@@ -26,6 +27,7 @@ class Tree:
         self.ax = self.fig.add_subplot(111)
         self.ax.set_xlim(x1, x2)
         self.ax.set_ylim(y1, y2)
+        self.ax.axis('equal')
 
         self.root_node = Node(x1, x2, y1, y2, self.ax)
 
@@ -67,8 +69,6 @@ class Node:
 
     def update(self, body: Body):
 
-        body.node_chain.append(self)
-
         if not self.is_occupied:
             self.mass = body.m
             self.mass_center_x = body.x
@@ -93,9 +93,63 @@ class Node:
                 self.leaf_body = None
 
 
+class ForceCalculator:
+
+    def __init__(self, tree: Tree, theta: float = 0.5):
+
+        self.tree = tree
+        self.theta = theta
+
+    def calculate_forces(self, bodies: List[Body]):
+        for body in bodies:
+            self.force_consideration(self.tree.root_node, body)
+
+    def force_consideration(self, node: Node, body: Body):
+
+        if node.is_occupied is False:
+            return
+
+        if node.leaf_body is body:
+            return
+
+        distance = ((node.mass_center_y - body.y) ** 2 + (node.mass_center_x - body.x) ** 2) ** 0.5
+
+        if (node.x2 - node.x1) / distance < self.theta:
+            Fx, Fy = self.gravitational_force(node, body)
+            body.Fx += Fx
+            body.Fy += Fy
+            return
+        else:
+            for children_node in node.children:
+                self.force_consideration(children_node, body)
+
+    @staticmethod
+    def gravitational_force(node: Node, body: Body):
+
+        G = 6.67e-11
+
+        M = node.mass
+        Rx = node.mass_center_x
+        Ry = node.mass_center_y
+
+        m = body.m
+        rx = body.x
+        ry = body.y
+
+        F = -G*M*m/(((Rx - rx)**2 + (Ry - ry)**2)**(3/2))
+        Fx = F*(rx - Rx)
+        Fy = F*(ry - Rx)
+
+        return Fx, Fy
+
+
 if __name__ == '__main__':
     import random
     bodies = [Body(1, random.uniform(0, 2), random.uniform(0, 2), 0, 0) for _ in range(100)]
     tree = Tree(0, 2, 0, 2)
     tree.update(bodies)
+    ForceCalculator(tree).calculate_forces(bodies)
+    for i, body in enumerate(bodies):
+        plt.arrow(body.x, body.y, body.Fx*1e7, body.Fy*1e7)
+        plt.scatter(body.x, body.y, color='black')
     plt.show()
